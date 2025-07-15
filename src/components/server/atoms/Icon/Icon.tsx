41f499c9283573798a8199679227bae3f26c0d44
hsilvaM <hsilva@muniguate.com>
2025-07-15 09:50:42 -0600

    [wip]: Components

---
+++ src/components/server/atoms/Icon/Icon.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import React from "react";
+import { IconMap, IconProps } from "./types";
+import styles from "./Icon.module.scss";
+import classNames from "classnames";
+
+/**
+ * Componente Icon que renderiza un ícono SVG basado en el nombre proporcionado.
+ *
+ * @param {string} className - Clases adicionales para aplicar al ícono.
+ * @param {string} name - El nombre del ícono a renderizar.
+ * @param {IconProps} props - Las propiedades adicionales para el ícono.
+ * @returns {JSX.Element} - El componente de ícono renderizado.
+ */
+const Icon: React.FC<IconProps> = ({ className, name, ...props }) => {
+  const SVGIcon = IconMap[name];
+  return (
+    <SVGIcon
+      className={classNames(className ? className : styles.icon)}
+      width={50}
+      height={50}
+      {...props}
+    />
+  );
+};
+
+export default Icon;

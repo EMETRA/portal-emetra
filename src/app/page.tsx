2c86a1b7485ea029344fb41dd4f63bc29fcd896a
hsilvaM <hsilva@muniguate.com>
2025-08-19 14:53:24 -0600

    [PVE-26] [fix] Deprecation errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
 import { Separator } from "@/components/atoms/Separator";
 import styles from "./page.module.css";
-import { NavBar } from "@/components/server/molecules/NavBar";
-import { Footer } from "@/components/server/molecules/Footer";
+import { Banner, BannerSlide, ServicesRow } from "@/components/organisms";
+import classNames from "classnames";
+import { Icon } from "@/components/server/atoms";
+import MultimediaCarrousel from "@/components/server/organisms/MultimediaCarrousel/MultimediaCarrousel";
+import MultimediaCard from "@/components/server/molecules/MultimediaCard/MultimediaCard";
+import CalendarWidget from "@/components/server/molecules/CalendarWidget/CalendarWidget";
+
+const slides: BannerSlide[] = [
+  {
+    backgroundImage: "/images/banner.jpg",
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    overlayImage: "/images/Logos.png",
+  },
+  {
+    backgroundImage: "/images/banner.jpg",
+    text: "Conoce nuestros servicios",
+    overlayImage: "/images/Logos.png",
+  },
+];
 
 export default function Home() {
   return (
@@ -10,9 +27,7 @@ export default function Home() {
       <Separator>
         <h1 className={classNames(styles.Title)}>Nuestros Servicios</h1>
       </Separator>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <ServicesRow />
-      </div>
+      <ServicesRow />
       <div className={classNames(styles.Services)}>
         <CalendarWidget />
         <div className={classNames(styles.VideosHeading)}>

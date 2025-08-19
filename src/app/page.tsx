b62a35311ff71c4a3c875ad0d260ab336ca3e47e
hsilvaM <hsilva@muniguate.com>
2025-08-19 15:35:01 -0600

    [PVE-26]: [feat]: News Carrousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import { Icon } from "@/components/server/atoms";
 import MultimediaCarrousel from "@/components/server/organisms/MultimediaCarrousel/MultimediaCarrousel";
 import MultimediaCard from "@/components/server/molecules/MultimediaCard/MultimediaCard";
 import CalendarWidget from "@/components/server/molecules/CalendarWidget/CalendarWidget";
+import { NewCard } from "@/components/molecules/NewCard";
+import NewsCarrousel from "@/components/organisms/NewsCarrousel/NewsCarrousel";
 
 const slides: BannerSlide[] = [
   {
@@ -64,6 +66,59 @@ export default function Home() {
           <h1 className={classNames(styles.Title)}>ÚLTIMAS NOTICIAS</h1>
         </div>
       </Separator>
+
+      <NewsCarrousel>
+        <NewCard
+          id="1"
+          title="Noticia 1"
+          date="19 de Agosto, 2025"
+          image="/images/Evento.jpg"
+        />
+        <NewCard
+          id="2"
+          title="Noticia 2"
+          date="18 de Agosto, 2025"
+          image="/images/Evento.jpg"
+        />
+        <NewCard
+          id="3"
+          title="Noticia 3"
+          date="17 de Agosto, 2025"
+          image="/images/Evento.jpg"
+        />
+        <NewCard
+          id="4"
+          title="Noticia 4"
+          date="16 de Agosto, 2025"
+          image="/images/Evento.jpg"
+        />
+        <NewCard
+          id="5"
+          title="Noticia 5"
+          date="15 de Agosto, 2025"
+          image="/images/Evento.jpg"
+        />
+        <NewCard
+          id="6"
+          title="Noticia 6"
+          date="14 de Agosto, 2025"
+          image="/images/Evento.jpg"
+        />
+        <NewCard
+          id="7"
+          title="Noticia 7"
+          date="13 de Agosto, 2025"
+          image="/images/Evento.jpg"
+        />
+
+        <NewCard
+          id="8"
+          title="Noticia 8"
+          date="12 de Agosto, 2025"
+          image="/images/Evento.jpg"
+        />
+        {/* ...más cards */}
+      </NewsCarrousel>
       <Separator>
         <h1 className={classNames(styles.Title)}>Preguntas Frecuentes</h1>
       </Separator>

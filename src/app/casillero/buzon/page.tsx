cd38edc9680a461e5e6a49eee732a49db0918264
eliasalvarado <77988653+eliasalvarado@users.noreply.github.com>
2026-06-22 11:36:54 -0600

    Feat: Vista Notificaciones (buzon)

---
+++ src/app/casillero/buzon/page.tsx
@@ -0,0 +1,10 @@
+import CasilleroDashboardLayout from "@/components/client/templates/CasilleroDashboardLayout/CasilleroDashboardLayout";
+import CasilleroBuzon from "@/components/client/organisms/CasilleroBuzon/CasilleroBuzon";
+
+export default function CasilleroBuzonPage() {
+  return (
+    <CasilleroDashboardLayout>
+      <CasilleroBuzon />
+    </CasilleroDashboardLayout> 
+  );
+}
